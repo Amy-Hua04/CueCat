24f2a1ded077ecd225bf6808067562a64bb1d858
Amy <amy.hua.ca@gmail.com>
2023-09-17 02:04:16 -0400

    Update main.ino

--- Arduino/Sketches/main/main.ino
+++ Arduino/Sketches/main/main.ino
@@ -2,8 +2,11 @@
 MCUFRIEND_kbv tft;
 
 #define BLACK   0x0000
-#define BLUE    0x001F
-#define RED     0xF800
+#define PINK    0xf7dedc
+#define LIGHT_BLUE 0xdcf1f7
+#define LIGHT_GREEN 0xe1f5d3
+#define BLUE    0x3e499e
+#define RED     0xc44537
 #define GREEN   0x07E0
 #define CYAN    0x07FF
 #define MAGENTA 0xF81F
